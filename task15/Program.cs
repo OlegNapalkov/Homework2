7a338f8a219c09897a3ad28bd7319fb2a2cb912c
Олег <nap1994@list.ru>
2022-07-14 19:22:49 +0300

    Задача с днями недели

---
+++ task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите цифру дня недели от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ if (number<8)
+ {
+  if (number<6)
+  {
+    Console.WriteLine("Данный день не является выходным, так что работаем, Солнце еще высоко");
+  }
+  else
+  {
+    Console.WriteLine("Ура, выходные!");
+  }
+ }
+ else
+ {
+    Console.WriteLine("Сказано же, введите число от 1 до 7");
+  }
