fc6c3941a8b2bf65548063eefa110c4ad6775244
Олег <nap1994@list.ru>
2022-07-14 19:28:13 +0300

    Задача со вторым числом

---
+++ task10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+    string number1 = number.ToString();
+    Console.WriteLine(number1[1]);
+
