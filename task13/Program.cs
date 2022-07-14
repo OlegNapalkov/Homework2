2718a7af7fa962543b2a1be45cda7385c9e5bd75
Олег <nap1994@list.ru>
2022-07-14 19:09:19 +0300

    Задача с третьей цифрой

---
+++ task13/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if(number<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+ else
+ {
+    string number1 = number.ToString();
+    Console.WriteLine(number1[2]);
+ }
+
+//  else 
+// {
+//  while (number>1000 && number>99)
+//  {
+//    number = number/10;  
+//  }
+//  int third = number % 10;
+//  Console.WriteLine(third); 
+// } 
